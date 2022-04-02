9ef4cc42f862d1238001ddc5bedb317ae9a29723
Tran Minh Hieu <tranhieu23.code@outlook.com>
2022-04-02 23:38:53 +0800

    make change to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,10 @@
+import { program } from "commander";
 import { startGRPCServer } from "./cmd/start_grpc_server";
+import { startKafkaConsumer } from "./cmd/start_kafka_consumer";
 
-startGRPCServer(".env");
+program.command("start_grpc_server").action(() => startGRPCServer(".env"));
+program
+    .command("start_kafka_consumer")
+    .action(() => startKafkaConsumer(".env"));
+
+program.parse();
