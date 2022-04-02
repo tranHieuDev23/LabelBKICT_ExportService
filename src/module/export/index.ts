7498c5b36777ae5c99922e0b17f3da0861e6f391
Tran Minh Hieu <tranhieu23.code@outlook.com>
2022-04-03 02:56:08 +0800

    add converters to brandi container

--- src/module/export/index.ts
+++ src/module/export/index.ts
@@ -5,6 +5,18 @@ import {
 } from "./dataset_exporter";
 import { ExcelExporterImpl, EXCEL_EXPORTER_TOKEN } from "./excel_exporter";
 import { ExportOperatorImpl, EXPORT_OPERATOR_TOKEN } from "./export_operator";
+import {
+    ImageProtoToImageConverterImpl,
+    IMAGE_PROTO_TO_IMAGE_CONVERTER_TOKEN,
+} from "./image_proto_to_image";
+import {
+    RegionProtoToRegionConverterImpl,
+    REGION_PROTO_TO_REGION_CONVERTER_TOKEN,
+} from "./region_proto_to_region";
+import {
+    UserIdToUserConverterImpl,
+    USER_ID_TO_USER_CONVERTER_TOKEN,
+} from "./user_id_to_user";
 
 export * from "./export_operator";
 
@@ -20,5 +32,17 @@ export function bindToContainer(container: Container): void {
     container
         .bind(EXCEL_EXPORTER_TOKEN)
         .toInstance(ExcelExporterImpl)
+        .inSingletonScope();
+    container
+        .bind(IMAGE_PROTO_TO_IMAGE_CONVERTER_TOKEN)
+        .toInstance(ImageProtoToImageConverterImpl)
+        .inSingletonScope();
+    container
+        .bind(REGION_PROTO_TO_REGION_CONVERTER_TOKEN)
+        .toInstance(RegionProtoToRegionConverterImpl)
+        .inSingletonScope();
+    container
+        .bind(USER_ID_TO_USER_CONVERTER_TOKEN)
+        .toInstance(UserIdToUserConverterImpl)
         .inSingletonScope();
 }
