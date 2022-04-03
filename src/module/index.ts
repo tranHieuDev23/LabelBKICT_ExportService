8731571deaf40d3c59c5bd8fa2146ea6571701f3
Tran Minh Hieu <tranhieu23.code@outlook.com>
2022-04-03 10:16:05 +0800

    fix missing info_providers initialization

--- src/module/index.ts
+++ src/module/index.ts
@@ -1,8 +1,10 @@
 import { Container } from "brandi";
 import * as exportModule from "./export";
 import * as exportManagementModule from "./export_management";
+import * as infoProvidersModule from "./info_providers";
 
 export function bindToContainer(container: Container): void {
     exportModule.bindToContainer(container);
     exportManagementModule.bindToContainer(container);
+    infoProvidersModule.bindToContainer(container);
 }
