262e870a83d2d7f5ad6c73942e84a8dff8fe1ee0
Trần Minh Hiếu <tranhieudev23@Trans-MacBook-Pro.local>
2023-07-20 22:38:51 +0700

    Update cron.d

--- deploy/cron.d
+++ deploy/cron.d
@@ -1,2 +1,2 @@
 # This command helps delete expired exports in the database every hour.
-0 */1 * * * node /build/dist/main.js --delete_expired_exports
+0 */1 * * * /usr/local/bin/node /build/dist/main.js --delete_expired_exports
